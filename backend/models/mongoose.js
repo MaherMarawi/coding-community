b0ccf591eb250a726755d6d7ca12a7dfc336e79e
MaherMarawi <mahermarawi92@gmail.com>
2023-02-19 10:45:46 +0100

    first commit

--- backend/models/mongoose.js
+++ backend/models/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const atlasDB = process.env.Db_URL
 
-mongoose.connect(atlasDB, {
+mongoose.connect("mongodb+srv://node-test:maher1992@cluster0.sboni.mongodb.net/matrixmaster?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
