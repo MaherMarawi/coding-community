e299e825dbcf0631413ad092426abe1b4bc66744
MaherMarawi <mahermarawi92@gmail.com>
2023-02-19 10:17:09 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -32,14 +32,14 @@ app.use(userRouter)
 app.use(commentRouter)
 app.use(mailRouter)
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 //listen to Port
 
