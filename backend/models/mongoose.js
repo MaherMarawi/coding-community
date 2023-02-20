0c4fd069126671ebc5bbe5d18760cc5e4cbd3d4c
MaherMarawi <mahermarawi92@gmail.com>
2023-02-21 00:53:27 +0100

    first commit

--- backend/models/mongoose.js
+++ backend/models/mongoose.js
@@ -2,11 +2,11 @@
 
 const mongoose = require('mongoose')
 
-const atlasDB = process.env.Db_URL
+const Db_URL = process.env.Db_URL
 const userName = process.env.userName
 const password = process.env.pass
 
-mongoose.connect("mongodb+srv://node-test:maher1992@cluster0.sboni.mongodb.net/matrixmaster?retryWrites=true&w=majority", {
+mongoose.connect(Db_URL, {
     useNewUrlParser: true,
     UseCreateIndexes: true,
     useUnifiedTopology: true
