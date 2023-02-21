de710eedc7ac5217535dd877ff72be82c7d1e0f9
MaherMarawi <mahermarawi92@gmail.com>
2023-02-22 00:17:28 +0100

    first commit

--- frontend/src/components/microcomponents/codeblock/CodeBlock.jsx
+++ frontend/src/components/microcomponents/codeblock/CodeBlock.jsx
@@ -1,35 +1,33 @@
 import "./codeblock.scss"
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dark, docco, lightfair, googlecode, a11yLight, a11yDark, dracula, tomorrowNightBlue, tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { DarkModeContext } from '../../../context/darkModeContext';
-import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
-import "prismjs/themes/prism-tomorrow.css";
-import "highlight.js/styles/github.css";
-import hljs from "highlight.js";
+import { tomorrowNight, docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { DarkModeContext } from "../../../context/darkModeContext";
 
-
-
-import Highlight from 'react-highlight'
 
 
 const CodeBlock = ({ children }) => {
 
   const darkMode = useContext(DarkModeContext)
+  const [color, setColor] = useState()
   useEffect(() => {
-    hljs.highlightAll();
+    if (darkMode) setColor("black")
+    else setColor("white")
   }, []);
   return (
-    <div className="code-block">
-
-      <SyntaxHighlighter language="javascript" showLineNumbers="true" wrapLongLines={true} style={docco}>
+    <div className="code-block-container">
+      <SyntaxHighlighter
+        className="code-block"
+        language="js"
+        style={tomorrowNight}
+        wrapLongLines={true}
+      >
         {children}
       </SyntaxHighlighter>
-      {/* <pre>
-        <code className="language-javascript">{children}</code>
-      </pre> */}
     </div>
-  );
+
+
+  )
 };
 
 export default CodeBlock
