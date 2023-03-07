18a6e7921ac035745f779cc43ddbed77a7f9d474
MaherMarawi <mahermarawi92@gmail.com>
2023-03-07 14:24:48 +0100

    first commit

--- backend/models/mongoose.js
+++ backend/models/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const uri = process.env.uri
 
-mongoose.connect("mongodb+srv://node-test:maher1992@cluster0.sboni.mongodb.net/matrixmaster?retryWrites=true&w=majority", {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     UseCreateIndexes: true,
     useUnifiedTopology: true
